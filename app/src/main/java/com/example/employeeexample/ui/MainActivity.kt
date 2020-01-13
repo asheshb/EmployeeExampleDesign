c63438b2f7041d8742bd0b791aa0e60488b543c0
Ashesh Bharadwaj <ppashesh@gmail.com>
2020-01-13 13:56:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/employeeexample/ui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.employeeexample.ui
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.employeeexample.R
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun createFile(context: Context, folder: String, ext: String): File {
+    val timeStamp: String =
+        SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+    val filesDir: File? = context.getExternalFilesDir(folder)
+    val newFile = File(filesDir, "$timeStamp.$ext")
+    newFile.createNewFile()
+    return newFile
+}
+
+
+fun Activity.showToast(msg: String, duration: Int = Toast.LENGTH_SHORT){
+    Toast.makeText(this, msg, duration).show()
+}
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
