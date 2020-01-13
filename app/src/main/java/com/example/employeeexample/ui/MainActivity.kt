29d1b3ff8f93c70be336a6ea88ba3ac3f79c5699
Ashesh Bharadwaj <ppashesh@gmail.com>
2020-01-13 21:57:52 +0530

    Initial commit

--- app/src/main/java/com/example/employeeexample/ui/MainActivity.kt
+++ app/src/main/java/com/example/employeeexample/ui/MainActivity.kt
@@ -6,6 +6,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.navigateUp
 import com.example.employeeexample.R
 import java.io.File
 import java.text.SimpleDateFormat
@@ -26,10 +30,26 @@ fun Activity.showToast(msg: String, duration: Int = Toast.LENGTH_SHORT){
 }
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setIcon(R.mipmap.ic_launcher)
+
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
